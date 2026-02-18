6d0155d3046454933fc1919ed70748e448f745d8
Ocean <144541279+oceanja@users.noreply.github.com>
2026-02-19 01:34:31 +0530

    Time: 0 ms (100%), Space: 34.5 MB (57.2%) - LeetHub

---
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> toggleLightBulbs(vector<int>& bulbs) {
+        int n=bulbs.size();
+        unordered_map<int,int> mpp;
+        vector<int> ans;
+
+        for(int i=0;i<n;i++){
+            mpp[bulbs[i]]++;
+        }
+
+        for(auto &it: mpp){
+            if((it.second)%2!=0){
+                ans.push_back(it.first);
+            }
+        }
+
+        sort(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
