7afacafadd1cb6a7b6b84deeadc9b600f193f722
Ocean <144541279+oceanja@users.noreply.github.com>
2026-02-18 15:42:00 +0530

    Time: 0 ms (100%), Space: 43.9 MB (21.18%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        int n=words.size();
+        string ans="";
+
+        vector<int> alphabets(26);
+
+        for(string &str:words){
+            int wt=0;
+            for(char c: str){
+                wt+=weights[c-'a'];
+            }
+
+            int newWt = wt % 26;
+            char mappedChar = 'z' - newWt;
+            ans.push_back(mappedChar);
+
+        }
+
+        return ans;
+        
+    }
+};
