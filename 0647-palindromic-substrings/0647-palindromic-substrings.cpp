8c8c56f46e8cf178d3721a4cf894047302a631af
Ocean <144541279+oceanja@users.noreply.github.com>
2026-02-20 23:33:07 +0530

    Time: 33 ms (27.09%), Space: 13 MB (39.01%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
+int t[1001][1001];
 bool isPalindrome(string &s,int left,int right){
-    while(left<right){
-        if(s[left]!=s[right]) return false;
-        left++;
-        right--;
+    if(left>=right){
+        return 1;
     }
 
-    return true;
+    if(t[left][right]!=-1){
+        return t[left][right];
+    }
+
+    if(s[left]==s[right]){
+        return t[left][right]=isPalindrome(s,left+1,right-1);
+    }
+
+    return t[left][right]=0;
+
 }
     int countSubstrings(string s) {
         int n=s.length();
         int cnt=0;
+        memset(t,-1,sizeof(t));
 
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
