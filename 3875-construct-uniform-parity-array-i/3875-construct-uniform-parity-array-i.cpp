bf8ff5163a6cc24bafce6b4f354ad26143c81e34
Ocean <144541279+oceanja@users.noreply.github.com>
2026-03-30 23:27:29 +0530

    Time: 0 ms (100%), Space: 30.2 MB (94.76%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n=nums1.size();
+
+        // nums1 of n distinct integers.
+        // You want to construct another array nums2 of length n such that the elements in nums2 are either all odd or all even.
+
+        // nums2[i] = nums1[i]
+//         nums2[i] = nums1[i] - nums1[j], for an index j != i
+
+//  nums1 = [2,3]
+//   
+return true;
+
+
+
+        
+    }
+};
