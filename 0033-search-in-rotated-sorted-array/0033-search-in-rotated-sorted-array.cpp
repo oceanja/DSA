adadd74e4f968d5544b66a22a2ddbc2ed70dfcef
Ocean <144541279+oceanja@users.noreply.github.com>
2026-05-22 21:45:40 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.43%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,18 +1,49 @@
 class Solution {
 public:
+
+int bs(vector<int>& nums, int low, int high, int target){
+
+    while(low<=high){
+        int mid= (low)+(high-low)/2;
+
+        if(nums[mid]==target){
+            return mid;
+        }
+
+        else if (nums[mid]>target){
+            high=mid-1;
+        }
+
+        else{
+            low=mid+1;
+        }
+    }
+
+    return -1;
+}
     int search(vector<int>& nums, int target) {
         int n=nums.size();
 
-        for(int i=0;i<n;i++){
-            if(target==nums[i]){
-                return i;
+        int partitionIdx=-1;
+
+        for(int i=0;i<n-1;i++){
+            if(nums[i+1]<nums[i]){
+                partitionIdx=i;
             }
-           
         }
 
-        return -1;
+       int ans1= bs(nums,partitionIdx+1,n-1,target);
+       int ans2= bs(nums,0,partitionIdx,target);
 
+       if(ans1!=-1){
+        return ans1;
+       }
 
+       if(ans2!=-1){
+        return ans2;
+       }
+
+       return -1;
         
     }
 };
