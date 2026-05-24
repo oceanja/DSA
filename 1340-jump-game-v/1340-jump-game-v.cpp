228857281fbffcf51c5eb0ec025150483ffc8b3c
Ocean <144541279+oceanja@users.noreply.github.com>
2026-05-24 23:02:58 +0530

    Time: 15 ms (45.57%), Space: 19.1 MB (56.42%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+int n;
+vector<int> t;
+
+int solve(vector<int>& arr, int i, int d){
+    int result=1;
+
+    if(t[i]!=-1){
+        return t[i];
+    }
+
+    //left 
+    for(int j=i-1;j>=max(0,i-d);j--){
+        if(arr[j]>=arr[i]) break;
+
+        result=max(result, 1+ solve(arr,j,d));
+    }
+
+    // right
+
+    for(int j=i+1;j<=min(n-1,i+d);j++){
+
+        if(arr[j]>=arr[i]) break;
+
+        result=max(result, 1+ solve(arr,j,d));
+
+    }
+
+    return t[i]=result;
+}
+    int maxJumps(vector<int>& arr, int d) {
+        n=arr.size();
+        t.resize(n+1,-1);
+
+        int result=1;
+
+        for(int i=0;i<n;i++){
+            result=max(result, solve(arr,i,d));
+        }
+
+        return result;
+        
+        
+    }
+};
