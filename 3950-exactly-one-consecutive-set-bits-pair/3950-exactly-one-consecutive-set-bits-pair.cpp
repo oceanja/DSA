7903bdf104ef5354fbb22bea24464c4f9eda5093
Ocean <144541279+oceanja@users.noreply.github.com>
2026-06-09 01:06:04 +0530

    Time: 3 ms (0%), Space: 8 MB (0%) - LeetHub

---
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool consecutiveSetBits(int n) {
+    string binary = bitset<32>(n).to_string();
+
+    int cnt = 0;
+
+    for(int i=0;i<binary.length()-1;i++){
+
+        if(binary[i]=='1' && binary[i+1]=='1'){
+            
+             cnt++;
+        }
+    }
+
+    return cnt==1;
+        
+    }
+};
