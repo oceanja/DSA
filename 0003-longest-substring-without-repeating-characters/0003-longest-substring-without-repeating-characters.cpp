35353bc43a50af5bbe3862d766451b994d0dd309
Ocean <144541279+oceanja@users.noreply.github.com>
2026-07-04 00:22:04 +0530

    Time: 11 ms (48.45%), Space: 14.4 MB (20.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n=s.length();
+
+        unordered_set<char> st;
         int maxLen=0;
+
         int i=0;
         int j=0;
-        unordered_set<char> st;
 
         while(j<n){
             if(st.find(s[j])==st.end()){
                 st.insert(s[j]);
                 maxLen=max(maxLen,j-i+1);
                 j++;
+
             }
 
             else{
-
                 st.erase(s[i]);
-            i++;
-
+                i++;
             }
-
-            
         }
 
         return maxLen;
