0facd3c1fd06231217ce2d57daf662e6311fe331
Ocean <144541279+oceanja@users.noreply.github.com>
2026-07-03 23:50:29 +0530

    Time: 0 ms (100%), Space: 62.9 MB (78.44%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size();
+
+        int i=0;
+        int j=n-1;
+
+        int maxArea=0;;
+
+        while(i<j){
+            
+            int w=j-i;
+            int h=min(height[i],height[j]);
+
+            int area=w*h;
+
+            maxArea=max(area,maxArea);
+
+            if(height[i]<height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+
+
+            
+        }
+
+        return maxArea;
+        
+    }
+};
